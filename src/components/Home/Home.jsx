e159d52c460d71c683a34d1d31aee27a67910a41
ARamos <Agustin.Ramos@endava.com>
2022-09-08 15:21:53 -0300

    Initial

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './homeStyles.css'
+import { WebcamCapture} from '../Webcam/Webcam'
+
+
+const Home = () => {
+
+
+    const submitForm = () => {
+        alert("Form submitted");
+    }
+
+
+    return (
+        <div className="home-container">
+            <div className="container">
+                <div className="text">
+                    <h1>Fill up this form!</h1>
+                    <form className="form">
+                        <WebcamCapture/>
+                        <button type="submit" id="login-button" onClick={(e) => submitForm(e)}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
